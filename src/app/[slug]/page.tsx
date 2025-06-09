e08cbd552753c114a384bf8bea57a8d9020ca14a
1402jelorand <jeanfebxl@hotmail.fr>
2025-06-10 00:16:07 +0200

    code safe vercel 2

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,12 +1,12 @@
 import { PortableText, type SanityDocument } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+// import imageUrlBuilder from "@sanity/image-url";
+// import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "@/sanity/client";
 import Link from "next/link";
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
-const { projectId, dataset } = client.config();
+// const { projectId, dataset } = client.config();
 
 const options = { next: { revalidate: 30 } };
 
